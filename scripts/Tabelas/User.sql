0d238fe43b5122a3bedb5b422230c1ee28ce901c
Livius Brasil <liviusbrasil@outlook.com>
2021-02-24 11:20:05 -0300

    Script de criação da tabela User

---
+++ scripts/Tabelas/User.sql
@@ -0,0 +1,27 @@
+USE [financ_general]
+GO
+
+/****** Object:  Table [dbo].[User]    Script Date: 24/02/2021 11:19:06 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[User]') AND type in (N'U'))
+DROP TABLE [dbo].[User]
+GO
+
+/****** Object:  Table [dbo].[User]    Script Date: 24/02/2021 11:19:06 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User](
+	[UserId] [bigint] NOT NULL,
+	[PersonId] [bigint] NOT NULL,
+	[Password] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
